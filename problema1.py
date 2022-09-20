5af842f81b3e3d294f953c59fe9955dcaeac4d03
Savca Mihaela <70644060+SavcaMihaela@users.noreply.github.com>
2022-09-20 22:08:07 +0300

    Add files via upload

---
+++ problema1.py
@@ -0,0 +1,48 @@
+a=int(input("Primul numar"))
+b=int(input("Al doilea numar"))
+## a) suma numerelor
+import math
+def sum():
+    return a+b
+    print("Suma",sum())
+## b) produsul numerelor
+def prod():
+    return a*b
+    print("Produsul", prod())
+## c) media aritmetica a numerelor
+def med_arit():
+    return (a+b)/z
+    print("Media aritmetica", med_arid())
+## d) cel mai mare divizor comun
+## e) cel mai mic multiplu comun
+## f) numarul minim
+def min():
+    if (a<b)
+    return min(a)
+    else:
+        return(b)
+    print("Minimul", min ())
+## g) numarul maxim
+def max():
+    if (a>b)
+    return max(a)
+    else:
+        return b
+    print("Maximul", max())
+## h) suma numerelor in formatul a+b=c, daca a si b reprezinta numere citite
+def suma2(a, b):
+    s = str(a) "+" +str(b)  "=" str(c) , str (suma (a,b))
+    return s (a,b)
+    print("suma", s ())
+## i) produsul numerol in formatul a*b=c unde a si b reprezinta numere citite
+def prod2(a, b):
+    prod = str(a)"*"str(b)"="str(c), str (suma (a,b))
+    return prod (a,b)
+    print ("produsul", prof())
+## j) Toti divizorii comuni
+## k) Cinci multipli comeni
+## l) cifrele care se contin in ambele numere
+## m) Cifrele care sunt in primul numar si nu sunt in al doilea
+## n) va afisa mesajul PRTIETENE , daca numerele sunt prietene. Doua numere se numesc prietene, daca numarul de divizori este acelasi
+
+
